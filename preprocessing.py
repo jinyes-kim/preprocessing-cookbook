4ff46945996bd6333001be698e0b6f8a166f55b5
Kim Jinung <54028026+jinyes-kim@users.noreply.github.com>
2020-07-03 15:10:23 +0900

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,28 @@
+# remove special character
+def rm_sp_char(list_data):
+    res_list = [''.join(x for x in word if x.isalnum()) for word in list_data]
+    return res_list
+
+
+# remove special character & number
+def rm_sp_char_num(list_data):
+    res_list = [''.join(x for x in word if x.isalpha()) for word in list_data]
+    return res_list
+
+
+# word count
+def word_count(list_data):
+    res_dict = {}
+    for word in list_data:
+        if word in res_dict:
+            res_dict[word] += 1
+        else:
+            res_dict[word] = 1
+
+    return res_dict
+
+
+# example
+sample = ['a!!123', 'a!', '!b', 'thisis###', 'c', 'c1']
+tmp = word_count(rm_sp_char_num(sample))
+print(tmp)
